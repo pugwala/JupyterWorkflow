9753bcc6fff9d615fbaf2ee3cfffc9b9b402d887
user <user@ubudesktop.lab.home>
2019-03-20 17:29:14 -0500

    BUG: fix bug in date parsing

--- jupyterworkflow/tests/test_data.py
+++ jupyterworkflow/tests/test_data.py
@@ -1,7 +1,9 @@
 from jupyterworkflow.data import get_freemont_data
 import pandas as pd
+import numpy as np
 
 def test_freemont_data():
     data = get_freemont_data()
     assert all(data.columns == ['West', 'East', 'Total'])
     assert isinstance(data.index, pd.DatetimeIndex)
+    assert len(np.unique(data.index.time) == 24)
